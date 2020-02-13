531bfcc239bfa62ac5fe2201fe8c14f9a73069f0
Zane <dawson50@purdue.edu>
2020-02-13 15:02:23 -0500

    start of project with header and start of main

---
+++ pa2/hcomp.h
@@ -0,0 +1,27 @@
+typedef struct HTree{ //huffman tree
+    char chr; //character for the node on the tree
+    struct HTree * left; //left pointer
+    struct HTree * right; //right pointer
+} Tree;
+
+typedef struct list{ //linked list to store frequency
+    char chr;
+    char* code;
+    long freq;
+    struct list * next;
+} list;
+
+
+//list functions ------------------------------------------------
+list* readFromFile(char* filename);
+list* addListNode(list* head, char chr, long freq);
+list* destroyList(list* head);
+void freqOutput(char* filename, list* head);
+//---------------------------------------------------------------
+
+//HTree functions -----------------------------------------------
+Tree* buildTree(Tree * root, list * head);
+void preTravHead(char* filename, Tree * root);
+list* preTravCode(char* filename, Tree* root);
+void codeMessage(char* inputFile, char* filename, Tree* root);
+//---------------------------------------------------------------
